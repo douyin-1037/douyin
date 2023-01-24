e43497fe8fe1d023243e3fabb77401fd61dfa7ff
BruceAko <dczdcz2001@aliyun.com>
2023-01-24 20:50:23 +0800

    finish api/handlers/like.go and add router

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"douyin/cmd/router"
 	"douyin/common/conf"
+	"github.com/cloudwego/kitex/pkg/klog"
 )
 
-func main() {
+func Init() {
 	conf.InitConfig()
+}
 
+func main() {
+	Init()
+	r := router.NewRouter()
+	if err := r.Run(conf.Server.HttpPort); err != nil {
+		klog.Fatal(err)
+	}
 }
