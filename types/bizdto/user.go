ac6d6488ee3e6bd3aec7c7391b01678d5cf588c0
UraraO <Haru_UraraO@outlook.com>
2023-01-29 10:56:32 +0800

    debug: adapt to new file directory

--- types/bizdto/user.go
+++ types/bizdto/user.go
@@ -72,12 +72,12 @@ type FanListResp struct {
 	UserList []*User `json:"user_list,omitempty"`
 }
 
-//好友列表
+// 好友列表
 type FriendListReq struct {
 	UserId int64 `form:"user_id" json:"user_id" binding:"required,max=32"`
 }
 
 type FriendListResp struct {
-	types.BaseResp
+	coredto.BaseResp
 	UserList []*User `json:"user_list,omitempty"`
 }
