66ed755adf983d8a3002602c8749c88fc61af852
winniterz <1907856+winniterz@users.noreply.github.com>
2023-02-06 18:55:50 +0800

    implement message application, rpc and service

--- gateway/application/message.go
+++ gateway/application/message.go
@@ -2,6 +2,9 @@ package application
 
 import (
 	"context"
+	"douyin/code_gen/kitex_gen/messageproto"
+	"douyin/gateway/rpc"
+	"github.com/pkg/errors"
 
 	"douyin/types/bizdto"
 )
@@ -16,11 +19,44 @@ func NewMessageAppService() *MessageAppService {
 }
 
 func (m MessageAppService) CreateMessage(ctx context.Context, appUserID int64, toUserID int64, content string) (err error) {
-	//TODO implement me
-	panic("implement me")
+	req := &messageproto.CreateMessageReq{
+		UserId:   appUserID,
+		ToUserId: toUserID,
+		Content:  content,
+	}
+	err = rpc.CreateMessage(ctx, req)
+	if err != nil {
+		return errors.Wrapf(err, "CreateMessage rpc failed, appUserID: %v, toUserID: %v, content: %v", appUserID, toUserID, content)
+	}
+	return nil
 }
 
 func (m MessageAppService) GetMessageList(ctx context.Context, appUserID int64, toUserID int64) (messageList []*bizdto.Message, err error) {
-	//TODO implement me
-	panic("implement me")
+	us, err := rpc.GetMessageList(ctx, &messageproto.GetMessageListReq{
+		UserId:   appUserID,
+		ToUserId: toUserID,
+	})
+	if err != nil {
+		return nil, errors.Wrapf(err, "GetMessageList rpc failed, appUserID: %v, toUserID: %v", appUserID, toUserID)
+	}
+	return toMessageDTOs(us), nil
+}
+
+func toMessageDTO(message *messageproto.MessageInfo) *bizdto.Message {
+	if message == nil {
+		return nil
+	}
+	return &bizdto.Message{
+		ID:         message.MessageId,
+		Content:    message.Content,
+		CreateTime: message.CreateTime,
+	}
+}
+
+func toMessageDTOs(messages []*messageproto.MessageInfo) []*bizdto.Message {
+	us := make([]*bizdto.Message, 0, len(messages))
+	for _, user := range messages {
+		us = append(us, toMessageDTO(user))
+	}
+	return us
 }
